2a6491999b00fbc9af7ba7966f4873435545a237
abeaduhh <adiba.labonno@gmail.com>
2025-01-14 15:34:58 -0600

    added instructions for the assignment

--- PA1.cpp
+++ PA1.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// line one is the 2 positive integers for the image the length is 27 and the width is 7
+// line 2 from position 0 to 10, if you need to print something print with U, from 15 to 25 it is H (based in the range of the x axis)
+// line 3 and 4, decode the given problem with the index and the position of the U and H
+// 0 to 4 is the first 4 values of line 4, so 0 10 15 25, for the 4th line it is n - 1
+
 int main() {
 
 
+    return 0;
 }
